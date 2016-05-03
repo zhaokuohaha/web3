7d72305f92995cb18b6c94035d572bfc1e1350f2
zhaokuo <1014336691@qq.com>
2016-05-03 13:49:19 +0800

    实现注册页面-客户端表单验证, 没有实现注册时用户密码加密

--- web3/Global.asax.cs
+++ web3/Global.asax.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using web3.Domain.Concrete;
 
 namespace web3
 {
@@ -13,6 +15,7 @@ namespace web3
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            Database.SetInitializer<EFDbContext>(null);
         }
     }
 }
